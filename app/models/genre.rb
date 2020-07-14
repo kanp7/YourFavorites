2f33488059b35e5bab6c69c5bf585f3fd0aca694
vagrant <vagrant@localhost.localdomain>
2020-07-14 08:07:03 +0000

    Fix seeds

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,4 @@
 class Genre < ApplicationRecord
 	has_many :genre_relations
+	has_many :categories, through: :genre_relations
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,44 @@
-User.create!(
-	email: "jiro@jiro.com",
-	name: "jiro",
+User.create!([
+	{
+	email: "test@test.com",
+	name: "test",
 	password: "password",
 	password_confirmation: "password"
-	)
+	},
+	{
+	email: "test2@test2.com",
+	name: "test2",
+	password: "password",
+	password_confirmation: "password"
+	}
+	])
 
-BookCategory.create!(
-	category: "小説"
-)
+BookCategory.create!([
+	{ category: "小説" },
+	{ category: "漫画" },
+	{ category: "雑誌" },
+	{ category: "ビジネス・自己啓発" },
+	{ category: "参考書・技術書" }
+	])
 
-BookCategory.create!(
-	category: "漫画"
-)
+MovieCategory.create!([
+	{ category: "映画" },
+	{ category: "アニメ" },
+	{ category: "ドラマ" }
+	])
 
-BookCategory.create!(
-	category: "ビジネス書"
-)
+BookGenre.create!([
+	{ genre: "ファンタジー" },
+	{ genre: "SF" },
+	{ genre: "文学" },
+	{ genre: "エンタメ" },
+	{ genre: "ミステリー" }
+	])
 
-MovieCategory.create!(
-	category: "映画"
-)
-
-MovieCategory.create!(
-	category: "アニメ"
-)
-
-MovieCategory.create!(
-	category: "ドラマ"
-)
+MovieGenre.create!([
+	{ genre: "アニメ" },
+	{ genre: "恋愛" },
+	{ genre: "ホラー" },
+	{ genre: "スポーツ" },
+	{ genre: "SF" }
+	])
