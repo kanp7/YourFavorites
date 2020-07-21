b883e7e4f0b1e5b1bcfb0d0ea3051f9211debcda
vagrant <vagrant@localhost.localdomain>
2020-07-21 11:09:53 +0000

    Improve フォロー機能を非同期化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,15 +2,13 @@ class RelationshipsController < ApplicationController
 	before_action :authenticate_user!
 
 	def create
-		user = User.find(params[:followed_id])
-		current_user.follow(user)
-		redirect_back(fallback_location: root_path)
+		@user = User.find(params[:followed_id])
+		current_user.follow(@user)
 	end
 
 	def destroy
-		user = Relationship.find(params[:id]).followed
-		current_user.unfollow(user)
-		redirect_back(fallback_location: root_path)
+		@user = Relationship.find(params[:id]).followed
+		current_user.unfollow(@user)
 	end
 
 end
