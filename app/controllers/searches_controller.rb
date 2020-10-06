d76e5ad7246f31e0b8d5dc13fda46a35887f62b7
kanp7 <tatsuya715tk@gmail.com>
2020-10-06 06:15:32 +0000

    Fix searchコントローラー内のパラメータ名を修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,7 @@ class SearchesController < ApplicationController
 	def search
 		@select_model = params[:select_model]
 		@word = params[:word]
-		if @select_model == '1'
+		if @select_model == 'user'
 			return if @word.blank?
 
 			@users = User.search(@word).page(params[:page])
